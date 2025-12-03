676636bea77518ee410dbcb003868a1bae6c7f74
OrlandoTellez <espadachinkinno@gmail.com>
2025-12-02 19:53:30 -0600

    learn: se aprende como devolver un json, y tambien acerca de las macro de atributos

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,61 @@
+use std::vec;
+
 // aqui vamos a hacer algo muy basico con axum, crear un servidor web
-use axum::{Router, routing::get};
+use axum::{Router, response::Json, routing::get};
+use serde::{Deserialize, Serialize};
+// use serde_json::{Value, json};
+use tokio::net::TcpListener;
+
+const PORT: &str = "3000";
+
+#[derive(Serialize, Deserialize)]
+pub struct User {
+    name: String,
+    age: u8,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", get(list_users));
+
+    let url = format!("0.0.0.0:{}", PORT);
+
+    // ojo qui es importante saber elconcepto de pasar propiedad, por eso el & es importante, porque no pasamos la propiedad, sino solo prestamos el valor.
+    let listener = TcpListener::bind(&url).await.unwrap();
+
+    println!("Server listener in http://{}", &url);
 
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "hello world"
+}
+
+// async fn list_userrs() -> Json<Value> {
+//     let new_user = json!({
+//         "name": String::from("Orlando"),
+//         "age": 20,
+//     });
+
+//     Json(new_user)
+// }
+
+async fn list_users() -> Json<Vec<User>> {
+    let users = vec![
+        User {
+            name: String::from("Orlando"),
+            age: 20,
+        },
+        User {
+            name: String::from("David"),
+            age: 21,
+        },
+    ];
+
+    Json(users)
 }
 
 // Nota: es aceraca deel unwrap, resulta que en producción no es tan recomendable porque practicamente le dice al programa que si falla haga un panic, hay mejores formas de manejar un error.
