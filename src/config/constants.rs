3bd38a5bef59a2d85ea2f03e0081204f5f13fcf5
OrlandoTellez <espadachinkinno@gmail.com>
2025-12-29 11:46:40 -0600

    refactor: mejorar logica de constants para que no falle en produccion el jwt_secret

--- src/config/constants.rs
+++ src/config/constants.rs
@@ -2,5 +2,7 @@ use std::env;
 
 use once_cell::sync::Lazy;
 
-pub static JWT_SECRET: Lazy<String> =
-    Lazy::new(|| env::var("JWT_SECRET").expect("Variable de entorno requerida"));
+pub static JWT_SECRET: Lazy<String> = Lazy::new(|| {
+    env::var("JWT_SECRET")
+        .unwrap_or_else(|_| "clave_super_secreta_jaja_usar_la_de_entorno_env".to_string())
+});
